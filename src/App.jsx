a0293d2a583060492d009e70c7643c6e8ef0e8ae
Suyash Jain <sj@Suyashs-MacBook-Air.local>
2024-10-25 00:49:28 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import SocialLinks from './components/socialLinks'
+import SocialLinks from './components/SocialLinks'
 import About from './components/About'
 import Portfolio from './components/portfolio'
 import Experience from './components/experience'
